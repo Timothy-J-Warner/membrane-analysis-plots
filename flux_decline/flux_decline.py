cae9b29db8b0c050e5fb9b4a1565622dc610b5bd
Timothy Warner <timothy.warner15@gmail.com>
2024-05-31 13:13:31 -0400

    Added a linear regression function to measure the rate of decline in flux.

--- flux_decline/flux_decline.py
+++ flux_decline/flux_decline.py
@@ -1,11 +1,21 @@
-# import numpy as np
+import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from scipy import stats
 
 plot_data = pd.read_csv('flux_decline_data.csv')
 
 time = plot_data['Time (mins)'].to_numpy()
 flux = [plot_data[f'Flux {i} (LMH)'].to_numpy() for i in range(3)]
+
+lin_reg = [stats.linregress(time, flux[i]) for i in range(3)]
+
+intercept = np.zeros(3)
+slope = np.zeros(3)
+
+for i in range(3):
+    intercept[i] = lin_reg[i].intercept
+    slope[i] = lin_reg[i].slope
 
 fig = plt.figure()
 first_line, = plt.plot(time, flux[0], c='#88ccee', label='Flux 0')
@@ -18,3 +28,10 @@ plt.axis((0, max(time), 0, max([max(flux[0]), max(flux[1]), max(flux[2])])*1.2))
 plt.savefig('flux_decline.svg')
 plt.savefig('flux_decline.pdf')
 plt.savefig('flux_decline.jpg', dpi=300)
+
+output_data = {
+    'Gradient (LMH/min)': slope
+}
+
+df_outputs = pd.DataFrame(output_data)
+df_outputs.to_csv("linear_flux_decline_rate.csv", index=False)
