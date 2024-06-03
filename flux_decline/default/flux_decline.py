487d57dd7a05c80c4ee13ba1ad3d2e57354f8280
Timothy Warner <timothy.warner15@gmail.com>
2024-06-03 17:02:16 -0400

    Continuing to implement exponential modelling of flux decline data.

--- flux_decline/default/flux_decline.py
+++ flux_decline/default/flux_decline.py
@@ -14,50 +14,24 @@ def func(x, a, b, c):
     return a * np.exp(-b * x) + c
 
 
-parameter_bounds = (0, [4000, np.inf, 4000])
+parameter_bounds = (0, [np.inf, np.inf, np.inf])
 initial_guess = [1500, 0.01, 2500]
 maxfev = 800
 
-
-popt, pcov = curve_fit(func, time, flux[0], p0=initial_guess, bounds=parameter_bounds, maxfev=maxfev)
-
-residuals = flux[0] - func(time, *popt)
-ss_res = np.sum(residuals**2)
-ss_tot = np.sum((flux[0]-np.mean(flux[0]))**2)
-r_squared = 1 - (ss_res / ss_tot)
-
-print(popt)
-print(r_squared)
-
-fig = plt.figure()
-
-first_line, = plt.plot(time, flux[0], 'o', c='#88ccee', label='Flux 0')
-second_line, = plt.plot(time, func(time, *popt), c='#ddcc77', label='Trend')
-
-
-plt.xlabel('Time (mins)')
-plt.ylabel(u'Flux (Lm\u207b\u00b2Hr\u207b\u00b9)')
-plt.axis((0, max(time), 0, max([max(flux[0]), max(flux[1]), max(flux[2])])*1.2))
-plt.savefig('results/test.jpg', dpi=300)
-
-plt.close()
-
-lin_reg = [stats.linregress(time, flux[i]) for i in range(3)]
-
-intercept = np.zeros(3)
-slope = np.zeros(3)
-percent_decline = np.zeros(3)
-flux_output = np.zeros(3)
+model_param = np.zeros([3, 3])
+model_r_squared = np.zeros(3)
 
 for i in range(3):
-    intercept[i] = lin_reg[i].intercept
-    slope[i] = lin_reg[i].slope
-    percent_decline[i] = slope[i] / flux[i][1] * 100
-    flux_output[i] = flux[i][0]
+    popt, pcov = curve_fit(func, time, flux[i], p0=initial_guess, bounds=parameter_bounds, maxfev=maxfev)
+    residuals = flux[i] - func(time, *popt)
+    ss_res = np.sum(residuals**2)
+    ss_tot = np.sum((flux[i]-np.mean(flux[i]))**2)
+    r_squared = 1 - (ss_res / ss_tot)
+    model_param[i] = popt
+    model_r_squared[i] = r_squared
 
-slope = np.append(slope, np.mean(slope))
-percent_decline = np.append(percent_decline, np.mean(percent_decline))
-flux_output = np.append(flux_output, np.mean(flux_output))
+model_r_squared = model_r_squared[:, np.newaxis]
+model_param = np.concatenate((model_param, model_r_squared), axis=1)
 
 fig = plt.figure()
 first_line, = plt.plot(time, flux[0], c='#88ccee', label='Flux 0')
@@ -66,17 +40,17 @@ third_line, = plt.plot(time, flux[2], c='#cc6677', label='Flux 2')
 plt.xlabel('Time (mins)')
 plt.ylabel(u'Flux (Lm\u207b\u00b2Hr\u207b\u00b9)')
 plt.axis((0, max(time), 0, max([max(flux[0]), max(flux[1]), max(flux[2])])*1.2))
-# plt.legend(loc='lower right')
+plt.legend(loc='lower right')
 plt.savefig('results/flux_decline.svg')
 plt.savefig('results/flux_decline.pdf')
 plt.savefig('results/flux_decline.jpg', dpi=300)
 
-output_description = ['1', '2', '3', 'Mean']
+output_description = ['a', 'b', 'c', 'R2']
 
 output_data = {
-    'Specimen': output_description, u'Initial Flux (Lm\u207b\u00b2Hr\u207b\u00b9/min)': flux_output, u'Gradient (Lm\u207b\u00b2Hr\u207b\u00b9/min)': slope,
-    'Flux Decline (%/min)': percent_decline
+    'Model Parameters': output_description, u'Specimen 1': model_param[0],
+    u'Specimen 2': model_param[1], u'Specimen 3': model_param[2]
 }
 
 df_outputs = pd.DataFrame(output_data)
-df_outputs.to_csv("results/experiment_information.csv", index=False)
+df_outputs.to_csv("results/model_parameters.csv", index=False)
